05f064b048ab6eae0e839828b0a608ff9282fca7
Luke <luke.mcewan@marleyspoon.com>
2021-11-19 15:49:21 +0100

    Initial Commit

---
+++ Stretch+Paws/Stretch_PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stretch_PawsApp.swift
+//  Stretch+Paws
+//
+//  Created by Luke McEwan on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Stretch_PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
